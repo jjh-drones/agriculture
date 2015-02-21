053e0d1f77a6c8caf8dd76df5452717c0741530d
jjh-drones <jjh.drones@gmail.com>
2015-02-20 22:40:52 -0600

    added some aileron feedback

--- Sizing Code/aero_sizing/aileron_sizing.m
+++ Sizing Code/aero_sizing/aileron_sizing.m
@@ -10,29 +10,47 @@ delta      = b_begginingb_perc - b_beggininga_perc;
 iterations = 1000;
 diff       = delta/iterations;
 G = 1;
-for i = b_beggininga_perc:diff:b_begginingb_perc,
-   [a(G),b(G),c(G)] = aileron_setting(wing.CLalpha,wing.croot*aileronC_ratio,wing.croot,i,b_end_max_perc,wing.S,wing.b,da_max,1.3^2*mission.q_second,mission.rho_second,hstab.S,vstab.S,cg.Ixx,mission.turn_angle); 
-%     aileron.t_roll(G) = a;
-%     aileron.Pss(G) = b;
-%     aileron.bank_angle_till_steady(G) = c;
-    size_ail(G) = b_end_max_perc - i;
-    ail_start(G) = i;
-    G = G + 1;
-end
-min_t = min(a);
-if mission.turn_sec < min_t,
-    aileron.t_roll = 0;
-    aileron.Pss = 0;
-    aileron.bank_angle_till_steady = 0;
-else
-    aileron.t_roll      = mission.turn_sec;
-    aileron.Pss         = interp1(a(:),b(:),aileron.t_roll);
-    aileron.bank_angle_till_steady = interp1(a(:),c(:),aileron.t_roll);
-    aileron.chord       = aileronC_ratio*wing.croot;
-    aileron.chord_perc  = aileronC_ratio;
-    aileron_b_perc      = interp1(a(:),size_ail(:),aileron.t_roll);
-    aileron.b_perc      = interp1(a(:),size_ail(:),aileron.t_roll);
-    aileron.span        = aileron_b_perc*wing.b/2;
-    aileron.start       = interp1(a(:),ail_start(:),aileron.t_roll)*wing.b/2;
+objective  = 0;
+while objective == 0,
+    for i = b_beggininga_perc:diff:b_begginingb_perc,
+       [a(G),b(G),c(G)] = aileron_setting(wing.CLalpha,wing.croot*aileronC_ratio,wing.croot,i,b_end_max_perc,wing.S,wing.b,da_max,1.3^2*mission.q_second,mission.rho_second,hstab.S,vstab.S,cg.Ixx,mission.turn_angle); 
+    %     aileron.t_roll(G) = a;
+    %     aileron.Pss(G) = b;
+    %     aileron.bank_angle_till_steady(G) = c;
+        size_ail(G) = b_end_max_perc - i;
+        ail_start(G) = i;
+        G = G + 1;
+    end
+    min_t = min(a);
+    if mission.turn_sec < min_t,
+        aileron.t_roll = 0;
+        aileron.Pss = 0;
+        aileron.bank_angle_till_steady = 0;
+        aileronC_ratio = aileronC_ratio + 0.01;
+        aileronC_ratio
+        if aileronC_ratio >= 0.5, %the closest possible alternative
+            aileron.t_roll      = mission.turn_sec;
+            aileron.Pss         = interp1(a(:),b(:),aileron.t_roll);
+            aileron.bank_angle_till_steady = interp1(a(:),c(:),aileron.t_roll);
+            aileron.chord       = aileronC_ratio*wing.croot;
+            aileron.chord_perc  = aileronC_ratio;
+            aileron_b_perc      = interp1(a(:),size_ail(:),aileron.t_roll);
+            aileron.b_perc      = interp1(a(:),size_ail(:),aileron.t_roll);
+            aileron.span        = aileron_b_perc*wing.b/2;
+            aileron.start       = interp1(a(:),ail_start(:),aileron.t_roll)*wing.b/2;
+            objective           = 8 %end search
+        end
+    else
+        aileron.t_roll      = mission.turn_sec;
+        aileron.Pss         = interp1(a(:),b(:),aileron.t_roll);
+        aileron.bank_angle_till_steady = interp1(a(:),c(:),aileron.t_roll);
+        aileron.chord       = aileronC_ratio*wing.croot;
+        aileron.chord_perc  = aileronC_ratio;
+        aileron_b_perc      = interp1(a(:),size_ail(:),aileron.t_roll);
+        aileron.b_perc      = interp1(a(:),size_ail(:),aileron.t_roll);
+        aileron.span        = aileron_b_perc*wing.b/2;
+        aileron.start       = interp1(a(:),ail_start(:),aileron.t_roll)*wing.b/2;
+        objective           = 8; %end search
+    end
 end
 end
